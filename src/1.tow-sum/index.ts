d41f118c54daa10824092ae8c4d34ffa45009550
SGoldL <sml782@163.com>
2020-06-08 22:20:41 +0800

    feat: int reverse

--- src/1.tow-sum/index.ts
+++ src/1.tow-sum/index.ts
@@ -1,14 +1,10 @@
-interface TwoNum {
-  (nums: number[], target: number): number[]
-};
-
 // 双层 for 循环
-export const twoSumFor1: TwoNum = (nums, target) => {
+export function twoSum1 (nums: number[], target: number): number[] {
   const lens = nums.length;
   for (let i = 0; i < lens - 1; i++) {
     const leftNum = nums[i];
+    const lastNum = target - leftNum;
     for (let j = i + 1; j < lens; j++) {
-      const lastNum = target - leftNum;
       const rightNum = nums[j];
       if (rightNum === lastNum) {
         return [i, j];
@@ -18,18 +14,40 @@ export const twoSumFor1: TwoNum = (nums, target) => {
   return [];
 };
 
-// 层 for 循环
-export const twoSumFor2: TwoNum = (nums, target) => {
+// 双层 while 循环
+export function twoSum2 (nums: number[], target: number): number[] {
   const lens = nums.length;
-  for (let i = 0; i < lens - 1; i++) {
+  let i = 0;
+  while (i < lens - 1) {
     const leftNum = nums[i];
-    for (let j = i + 1; j < lens; j++) {
-      const lastNum = target - leftNum;
+    const lastNum = target - leftNum;
+    let j = i + 1;
+    while (j < lens) {
       const rightNum = nums[j];
       if (rightNum === lastNum) {
         return [i, j];
       }
+      j++;
     }
+    i++;
+  }
+  return [];
+};
+
+// 单层循环+借助辅助工具
+export function twoSum3 (nums: number[], target: number): number[] {
+  const numWeek = new Map();
+  const lens = nums.length;
+  let i = 0;
+  while (i < lens) {
+    const leftNum = nums[i];
+    const lastNum = target - leftNum;
+    if (numWeek.has(lastNum)) {
+      return [Math.min.apply(null, numWeek.get(lastNum)), i];
+    }
+    const lastIndexs = numWeek.get(lastNum) || [];
+    numWeek.set(leftNum, lastIndexs.concat(i));
+    i++;
   }
   return [];
 };
--- src/__tests__/1.tow-sum.test.ts
+++ src/__tests__/1.tow-sum.test.ts
@@ -1,21 +1,31 @@
-import { twoSumFor1 } from '../1.tow-sum';
+import { twoSum1, twoSum2, twoSum3 } from '../1.tow-sum';
 
-interface TestExp {
-  input: [number[], number];
-  output: number[];
-}
+type TestExp =  [number[], number, number[]];
 
 const testList: TestExp[] = [
-  {
-    input: [[2,7,11,15], 9],
-    output: [0, 1],
-  },
+  [[2,7,11,15], 9, [0, 1]],
+  [[3,2,4], 6, [1, 2]],
 ];
 
 describe('两数字之和', () => {
-  test('双层 for 循环', () => {
-    testList.forEach(({ input, output }) => {
-      expect(twoSumFor1(...input)).toEqual(output);
+  // 双层 for 循环
+  describe('双层 for 循环', () => {
+    test.each(testList)('(%o, %i): %o', (nums, target, res) => {
+      expect(twoSum1(nums, target)).toEqual(res);
+    });
+  });
+
+  // 使用 while 循环
+  describe('使用 while 循环', () => {
+    test.each(testList)('(%o, %i): %o', (nums, target, res) => {
+      expect(twoSum2(nums, target)).toEqual(res);
+    });
+  });
+
+  // 单层循环+借助辅助工具
+  describe('单层循环+借助辅助工具', () => {
+    test.each(testList)('(%o, %i): %o', (nums, target, res) => {
+      expect(twoSum3(nums, target)).toEqual(res);
     });
   });
 });
