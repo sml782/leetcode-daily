4de90cff93f91f255b320aff17edd948f3a4533e
sml782 <sml782@163.com>
2020-06-12 10:48:30 +0800

    feat(description): 调整题目描述

--- src/array/26.remove-duplicates.ts
+++ src/array/26.remove-duplicates.ts
@@ -1,3 +1,10 @@
+/**
+ * 26. 删除排序数组中的重复项
+ * 
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
+
 // NOTE: 1.splice 删除, 删除一个会让后面元素都往前移动
 export function removeDuplicates(nums: number[]): number {
   let lens = nums.length;
--- src/array/4.find-median-sorted-arrays.ts
+++ src/array/4.find-median-sorted-arrays.ts
@@ -1,9 +1,10 @@
-
-/*
-给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
-请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
-你可以假设 nums1 和 nums2 不会同时为空。
-*/
+/**
+ * 4. 寻找两个正序数组的中位数
+ * 
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+ * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ */
 
 // NOTE: 1.拼接后排序再求值
 export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
