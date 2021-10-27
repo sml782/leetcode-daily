c5a8aeb2fedc94245a651117128a6f30dab6c29e
sml782 <sml782@163.com>
2021-10-27 21:21:38 +0800

    feat: 动态规划专题第一天

---
+++ src/dynamic-programming/1137.n-th-tribonacci-number.ts
@@ -0,0 +1,86 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-27 20:55:57
+ * @LastEditTime: 2021-10-27 21:09:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode-daily/src/dynamic-programming/1137.n-th-tribonacci-number.ts
+ */
+/**
+ * DP 1137. 第 N 个泰波那契数
+ * 
+ * 泰波那契序列 Tn 定义如下： 
+ * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 T(n+3) = Tn + T(n+1) + T(n+2)
+ * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+ * 
+ * @example 1
+ * 
+ * 输入：n = 4
+ * 输出：4
+ * 解释：
+ * T_3 = 0 + 1 + 1 = 2
+ * T_4 = 1 + 1 + 2 = 4
+ * 
+ * @example 2
+ * 
+ * 输入：n = 25
+ * 输出：1389537
+ * 
+ * @tip
+ * 
+ * 0 <= n <= 37
+ * 答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
+ */
+
+
+/**
+ * 递归实现
+ * 
+ * ! 超时
+ *
+ * @export
+ * @param {number} n
+ * @returns {number}
+ */
+export function tribonacci(n: number): number {
+  if (n < 2) {
+    return n;
+  }
+
+  if (n === 2) {
+    return 1;
+  }
+
+  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+};
+
+/**
+ * 循环实现
+ *
+ * @export
+ * @param {number} n
+ * @returns {number}
+ */
+export function tribonacci1(n: number): number {
+  if (n < 2) {
+    return n;
+  }
+
+  if (n === 2) {
+    return 1;
+  }
+
+  let p = 0;
+  let q = 0;
+  let r = 1;
+  let s = 1;
+
+  for (let i = 3; i <= n; i++) {
+    p = q;
+    q = r;
+    r = s;
+    s = p + q + r;
+  }
+
+  return s;
+};
---
+++ src/dynamic-programming/509.fibonacci-number.ts
@@ -0,0 +1,67 @@
+/**
+ * DP 509. 斐波那契数
+ * 
+ * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。
+ * 该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给你 n ，请计算 F(n) 。
+ * 
+ * @example 1
+ * 
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ * 
+ * @example 2
+ * 
+ * 输入：3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ * 
+ * @example 3
+ * 
+ * 输入：4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ */
+
+/**
+ * 递归实现
+ *
+ * @export
+ * @param {number} n
+ * @returns {number}
+ */
+export function fib(n: number): number {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+
+
+/**
+ * 循环实现
+ *
+ * @export
+ * @param {number} n
+ * @returns {number}
+ */
+export function fib1(n: number): number {
+  if (n < 2) {
+    return n;
+  }
+  
+  let p = 0;
+  let q = 0;
+  let r = 1;
+
+  for (let i = 2; i <= n; i++) {
+    p = q;
+    q = r;
+    r = p + q;
+  }
+
+  return r;
+};
