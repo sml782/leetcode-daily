c052620e59acbde7a1028e39f8b7097da3b06d8d
亖巠 <sml782@163.com>
2022-02-05 21:51:42 +0800

    feat: 860. 柠檬水找零

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@
 // import { generateBT } from './lib/binary-tree';
 // import { createListNodeByArray } from './lib/linked-list';
 
-import { buddyStrings } from './2022-new-plan/859.buddy-strings';
+import { lemonadeChange } from './2022-new-plan/860.lemonade-change';
 
 // const bt = generateBT([3,4,5,-7,-6,null,null,-7,null,-5,null,null,null,-4]);
 
 // const head = createListNodeByArray([1, 2]);
 
-const res = buddyStrings('abcd', 'badc');
+const res = lemonadeChange([5,5,5,10,20]);
 
 console.dir(res);
